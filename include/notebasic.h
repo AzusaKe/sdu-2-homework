71af8faf33e06a57c6938100f8a34b826ab52a2d
Azusa_Ke <zhuo9_9@outlook.com>
2025-05-13 20:21:05 +0800

    基类构建

--- include/notebasic.h
+++ include/notebasic.h
@@ -5,4 +5,15 @@
 #ifndef NOTEBASIC_H
 #define NOTEBASIC_H
 
+using namespace std;
+
+class notebasic {
+public:
+    virtual void load_from_file(const string& filepath) = 0;
+    virtual void save_to_file(const string& filepath) = 0;
+    virtual void display() const = 0;
+    virtual string get_type() const = 0;
+    virtual ~notebasic() = default;
+};
+
 #endif //NOTEBASIC_H
--- src/notebasic.cpp
+++ src/notebasic.cpp
@@ -2,4 +2,6 @@
 // Created by Azusa_Ke on 25-5-13.
 //
 
+#include<string>
 #include "notebasic.h"
+
