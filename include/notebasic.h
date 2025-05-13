74b9fef4293fa3dc6a3c1009bf97e47caef99f31
Azusa_Ke <zhuo9_9@outlook.com>
2025-05-13 19:47:33 +0800

    添加基类

---
+++ include/notebasic.h
@@ -0,0 +1,8 @@
+//
+// Created by Azusa_Ke on 25-5-13.
+//
+
+#ifndef NOTEBASIC_H
+#define NOTEBASIC_H
+
+#endif //NOTEBASIC_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
+#include"fileoperater.h"
+#include"financenote.h"
+#include"passwordmanager.h"
+#include"reminder.h"
+
 using namespace std;
+
 int main() {
-    cout << "Hello World!!" << endl;
+
     return 0;
 }
