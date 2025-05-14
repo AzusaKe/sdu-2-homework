fc14c9dd7150411e7fa5ba4f5388ac12af9199fc
Azusa_Ke <zhuo9_9@outlook.com>
2025-05-14 15:24:07 +0800

    cmake

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@
 #include"filecheck_and_init.h"
 using namespace std;
 
-int main() {
+int main(bool graphics = false) {
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
     filecheck_and_init();
-    while (true) {
+    while (graphics != true) {
         cout << "请选择你要使用的工具：" << endl << "1.记账本" << endl << "选择数字并按下回车(为0则退出)：" << endl;
         int choice;
         cin >> choice;
