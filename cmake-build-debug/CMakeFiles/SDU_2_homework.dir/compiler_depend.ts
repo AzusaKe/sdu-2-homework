e0e81ebc3ba701bf0a7f2e32d20d417f416bf485
azusake <zhuo9_9@outlook.com>
2025-05-14 12:07:16 +0800

    加入linux兼容代码

---
+++ cmake-build-debug/CMakeFiles/SDU_2_homework.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for SDU_2_homework.
---
+++ cmake-build-debug/CMakeFiles/SDU_2_homework.dir/link.d
@@ -0,0 +1,61 @@
+SDU_2_homework: \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginT.o \
+  CMakeFiles/SDU_2_homework.dir/main.cpp.o \
+  CMakeFiles/SDU_2_homework.dir/src/filecheck_and_init.cpp.o \
+  CMakeFiles/SDU_2_homework.dir/src/financenote.cpp.o \
+  CMakeFiles/SDU_2_homework.dir/src/notebasic.cpp.o \
+  CMakeFiles/SDU_2_homework.dir/src/passwordmanager.cpp.o \
+  CMakeFiles/SDU_2_homework.dir/src/reminder.cpp.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.a \
+  /usr/lib/x86_64-linux-gnu/libm-2.39.a \
+  /usr/lib/x86_64-linux-gnu/libmvec.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_eh.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtend.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginT.o:
+
+CMakeFiles/SDU_2_homework.dir/main.cpp.o:
+
+CMakeFiles/SDU_2_homework.dir/src/filecheck_and_init.cpp.o:
+
+CMakeFiles/SDU_2_homework.dir/src/financenote.cpp.o:
+
+CMakeFiles/SDU_2_homework.dir/src/notebasic.cpp.o:
+
+CMakeFiles/SDU_2_homework.dir/src/passwordmanager.cpp.o:
+
+CMakeFiles/SDU_2_homework.dir/src/reminder.cpp.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.a:
+
+/usr/lib/x86_64-linux-gnu/libm-2.39.a:
+
+/usr/lib/x86_64-linux-gnu/libmvec.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_eh.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtend.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o:
