17920a63409d0d697eb87312aaf9837814a16eed
Azusa_Ke <zhuo9_9@outlook.com>
2025-06-04 10:41:28 +0800

    修改

--- src/passwordmanager_window.cpp
+++ src/passwordmanager_window.cpp
@@ -8,6 +8,8 @@ Passwordmanager_Window::Passwordmanager_Window(QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle("桌面工具-密码管理器");
+    ui->passwordmanager_record_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->passwordmanager_record_table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     connect(ui->exit_Button,&QPushButton::clicked,this, &Passwordmanager_Window::close);
 }
 
@@ -19,6 +21,12 @@ Passwordmanager_Window::~Passwordmanager_Window()
 //设置指针函数
 void Passwordmanager_Window::set_ptr(passwordmanager *ptr) {
     password_manager_ptr_4 = ptr;
+    if (!password_manager_ptr_4) {
+        *(azusa_log::log) << "Error: 密码管理器指针为空!" << endl;
+        return;
+    }
+    password_manager_ptr_4->search("");
+    password_manager_ptr_4->sort(); // 确保数据已排序
 }
 
 void Passwordmanager_Window::closeEvent(QCloseEvent *event) {
